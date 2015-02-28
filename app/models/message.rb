647cda6a4ba181bcd8d102883b74440dfdb52760
Julia <julia.ryanx@gmail.com>
2015-02-28 12:37:01 +1100

    add user and message associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,4 +12,6 @@
 #
 
 class Message < ActiveRecord::Base
+  belongs_to :sender, :class_name => 'User'
+  belongs_to :receiver, :class_name => 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,7 @@
 class User < ActiveRecord::Base
     has_many :moments
     has_many :likes 
+    
+    has_many :messages_received, :class_name => 'Message', :foreign_key => 'receiver_id'
+    has_many :messages_sent, :class_name => 'Message', :foreign_key => 'sender_id'
 end
