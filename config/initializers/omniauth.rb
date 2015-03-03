3b6f3fa40af49abfdd715cdf11d624681f0d2ca0
Julia <julia.ryanx@gmail.com>
2015-03-03 11:25:38 +1100

    add user authentication validation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["facebook_app_id"], ENV["facebook_api_secret_key"],
-  :scope => 'email,user_birthday'
+  :scope => 'email'
 end
