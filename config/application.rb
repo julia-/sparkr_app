3cb0d938b6eab70ee11fae086698a2e9d826cecc
Einar Tufte-Johnsen <einartufte-johnsen@Einar-Tufte-Johnsens-MacBook-Pro.local>
2015-03-04 16:31:27 +1100

    config middleware

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module SparkrApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.delete Rack::Lock
+    config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
   end
 end
